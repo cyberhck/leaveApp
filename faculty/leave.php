1e0d9f9053271a085b8698840520e2f8f66d219a
Nishchal Gautam <gautam.nishchal@gmail.com>
2015-05-18 10:37:09 +0530

    leave.php just works

--- faculty/leave.php
+++ faculty/leave.php
@@ -1,7 +1,52 @@
 <?php require_once("hackproof.php"); ?>
-<?php if (isset($_GET['submit'])) {
-	var_dump($_GET);
-	# code...
+<?php if (isset($_POST['submit'])) {
+	require_once("../config.php");
+	$con					=	mysql_connect($DB_HOST,$DB_USER,$DB_PASS);
+	$username				=	mysql_real_escape_string($_COOKIE['username']);
+	$sql					=	"SELECT * FROM `$DB_NAME`.`faculty` WHERE `username`='$username'";
+	$data					=	mysql_fetch_object(mysql_query($sql));
+	$applicantUsername		=	$username;
+	$leaveFor				=	mysql_real_escape_string($_POST['for']);
+	$leaveFrom				=	mysql_real_escape_string($_POST['from']);
+	$leaveTo				=	mysql_real_escape_string($_POST['to']);
+	$leaveAddress			=	mysql_real_escape_string($_POST['leaveAddress']);
+	$phoneNumberRes			=	mysql_real_escape_string($_POST['resPhone']);
+	$phoneNumberMob			=	mysql_real_escape_string($_POST['mobilePhone']);
+	$leavingCampusDate		=	mysql_real_escape_string($_POST['leavingCampusDate']);
+	$leavingCampusTime		=	mysql_real_escape_string($_POST['leavingCampusTime']);
+	$alternateTheory		=	mysql_real_escape_string($_POST['alternateTheory']);
+	$alternateLab			=	mysql_real_escape_string($_POST['alternateLab']);
+	$joiningCampusDate		=	mysql_real_escape_string($_POST['joiningCampusDate']);
+	$joiningCampusTime		=	mysql_real_escape_string($_POST['joiningCampusDate']);
+	$people					=	$_POST['people'];//array
+	$sql					=	"INSERT INTO `$DB_NAME`.`leaves` (`applicationFor`, `leaveFrom`, `leaveTo`, `leaveAddr`, `resPhone`, `mobPhone`, `leavingCampusDate`, `leavingCampusTime`, `joiningCampusDate`, `joiningCampusTime`, `alternateTheory`, `alternateLab`, `user`) VALUES('$leaveFor','$leaveFrom','$leaveTo','$leaveAddress','$phoneNumberRes','$phoneNumberMob','$leavingCampusDate','$leavingCampusTime', '$joiningCampusDate', '$joiningCampusTime','$alternateTheory','$alternateLab',$data->id);";
+		//insert that then look for people thingy
+	if(!mysql_query($sql)){
+	 	die("Some error occured, please try again later");
+	}
+	//tag the faculties.
+	$lastInsert				=	mysql_insert_id();
+	$sql					=	"INSERT INTO `$DB_NAME`.`tags`(`faculty`,`leaveId`) VALUES ";
+
+	require_once("functions.php");
+	
+	$DB_INFO['user']		=	$DB_USER;
+	$DB_INFO['pass']		=	$DB_PASS;
+	$DB_INFO['host']		=	$DB_HOST;
+	$DB_INFO['name']		=	$DB_NAME;
+	$str					=	[];
+	foreach ($people as $key) {
+		//echo $key;
+		$value				=	getFacultyIdFromUsername($DB_INFO,$key);
+		$str[]				=	"($value,$lastInsert)";
+	}
+	$str=implode(",", $str);
+	$sql=$sql.$str;
+	if(mysql_query($sql)){
+		header("location:ok.php?status=success");
+	}else{
+		die(mysql_error());
+	}
 }else{
 ?>
 <!DOCTYPE html>
@@ -22,47 +67,6 @@
 		<script src="/dist/js/modernizr.custom.js"></script>
 		<script src="/dist/js/nlform.js"></script>
 		<script src="/dist/leave.js"></script>
-		<style>
-		textarea::-webkit-input-placeholder {
-			color: rgba(255,255,255,0.8);
-		}
-
-		textarea:active::-webkit-input-placeholder ,
-		textarea:focus::-webkit-input-placeholder {
-			color: rgba(255,255,255,0.2);
-		}
-
-		textarea::-moz-placeholder {
-			color: rgba(255,255,255,0.8);
-		}
-
-		textarea:active::-moz-placeholder,
-		textarea:focus::-moz-placeholder {
-			color: rgba(255,255,255,0.2);
-		}
-
-		textarea:-ms-input-placeholder {  
-			color: rgba(255,255,255,0.8);
-		}
-
-		textarea:active::-ms-input-placeholder ,
-		textarea:focus::-ms-input-placeholder {
-			color: rgba(255,255,255,0.2);
-		}
-		textarea{
-			line-height: inherit;
-			display: inline-block;
-			color: #b14943;
-			padding:10px;
-			cursor: pointer;
-			border: 1px dashed #b14943;
-			background: transparent;
-			font-family: inherit;
-			font-size: inherit;
-			margin: 0;
-			-webkit-appearance: none;
-		}
-</style>
 	</head>
 	<body>
 		<?php
@@ -72,25 +76,24 @@
 			$sql				=	"SELECT * FROM `$DB_NAME`.`faculty` WHERE `username`='$username'";
 			$data				=	mysql_fetch_object(mysql_query($sql));
 		?>
-		<form id="nl-form" style="font-size:32px;margin-bottom:30px; margin-top:50px; width:1280px" class="nl-form">
+		<form id="nl-form" style="font-size:32px;margin-bottom:30px; margin-top:50px; width:1280px" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" class="nl-form">
 					Application for:
-					<select>
-						<option value="1" selected>CL</option>
-						<option value="2">SCL</option>
-						<option value="3">VL</option>
-						<option value="4">ML</option>
-						<option value="5">OOD</option>
-						<option value="6">C-OFF</option>
-						<option value="7">EL</option>
+					<select name="for">
+						<option value="CL" selected>CL</option>
+						<option value="SCL">SCL</option>
+						<option value="VL">VL</option>
+						<option value="ML">ML</option>
+						<option value="OOD">OOD</option>
+						<option value="C-OFF">C-OFF</option>
+						<option value="EL">EL</option>
 					</select><br />
 					<label>1. Name
-					<input size="18" data-subline="Your Username" type="text" placeholder="Username" name="name" value="<?php echo $data->fullname; ?>" disabled /><br /></label>
-					&nbsp;&nbsp;&nbsp;Designation:<input type="text" name="designation" value="<?php echo $data->designation; ?>" disabled /><br />
-					&nbsp;&nbsp;&nbsp;Department: <input type="text" name="department" value="<?php echo $data->department; ?>" disabled /><br />
+					<input size="18" data-subline="Your Username" type="text" placeholder="Username" name="name" value="<?php echo $data->fullname; ?>" readonly /><br /></label>
+					&nbsp;&nbsp;&nbsp;Designation:<input type="text" name="designation" value="<?php echo $data->designation; ?>" readonly /><br />
+					&nbsp;&nbsp;&nbsp;Department: <input type="text" name="department" value="<?php echo $data->department; ?>" readonly /><br />
 					2. Leave from <input type="date" name="from" /> to <input type="date" name="to" /><br />
 					3. Leave Address <input type="text" name="leaveAddress" /><br />
 					4. Phone No : Res <input type="text" size="14" name="resPhone"> Mobile <input type="text" size="14" name="mobilePhone"><br>
-					
 					5. a)Leaving the campus: <br>
 					&nbsp;&nbsp;&nbsp;&nbsp;At <input type="date" name="leavingCampusDate">On: <input size="7" type="text" name="leavingCampusTime"><br>
 					5. b)Expected to be back: <br>
@@ -98,12 +101,12 @@
 
 					6. Alternate Arrangements: <br>
 
-					<textarea name="" placeholder="For theory" cols="35" rows="4"></textarea> <textarea placeholder="For lab" name="" cols="35" rows="4"></textarea><br>
+					<textarea name="alternateTheory" placeholder="For theory" cols="35" rows="4"></textarea> <textarea placeholder="For lab" name="alternateLab" cols="35" rows="4"></textarea><br>
 					7. Send Notification to: <br>
 					<select multiple style="width:500px;font-size:14px;" name="people[]" id="friend_list">
 						<?php
 							$department		=		mysql_real_escape_string($data->department);
-							$sql			=		"SELECT * FROM `$DB_NAME`.`faculty` WHERE `department`='$department'";
+							$sql			=		"SELECT * FROM `$DB_NAME`.`faculty` WHERE `department`='$department' AND username !='$data->username';";
 							$query			=		mysql_query($sql);
 							while($result	=		mysql_fetch_object($query)){
 								?>
